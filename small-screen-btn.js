2794ca64212a6c5ca058d0bc8f7f276d486b819d
BlackAimZard <128994184+BlackAimZard@users.noreply.github.com>
2026-06-15 11:27:36 +0700

    Add files via upload

--- small-screen-btn.js
+++ small-screen-btn.js
@@ -1,4 +1,4 @@
-document.body.insertAdjacentHTML('beforeend', '<button id="mBtn" style="display:none; position:fixed; bottom:20px; right:20px; z-index:9999; width:45px; height:45px; padding:0; text-align:center; line-height:45px; background:#003399; color:#fff; border:none; border-radius:5px; font-size:24px; cursor:pointer; box-shadow:0 2px 5px rgba(0,0,0,0.3); -webkit-appearance:none; appearance:none;">☰</button>');
+document.body.insertAdjacentHTML('beforeend', '<button id="mBtn" style="display:none; position:fixed; bottom:20px; right:20px; z-index:9999; width:45px; height:45px; padding:0; text-align:center; line-height:45px; background:#000000; color:#fff; border:none; border-radius:5px; font-size:24px; cursor:pointer; box-shadow:0 2px 5px rgba(0,0,0,0.3); -webkit-appearance:none; appearance:none;">☰</button>');
 
 // LOGIC
 var btn = document.getElementById('mBtn');
