6525836863ffcee6345ecd79abddfcaa763b7ce5
BlackAimZard <128994184+BlackAimZard@users.noreply.github.com>
2026-07-06 16:44:42 +0700

    Add files via upload

---
+++ timeline-data.js
@@ -0,0 +1,77 @@
+const TIMELINE_HTML = `
+<div class="tl-grid">
+    <div class="tl-col">
+        <div class="tl-year-text">1991</div>
+        <div class="tl-nodes-container">
+            <a href="single.html#sg01" class="tl-node" draggable="false"
+               data-thumb="assets/timelines-photo/sg/sg01.jpg"
+               data-title="1st Single"
+               data-sub="Good-bye My Loneliness"
+               data-released="1991.02.10"
+               data-label="PODH-1029"
+			   data-tieup='"Kekkon no Risou to Genjitsu" Theme Song'>
+                <img src="assets/timelines-photo/sg/sg-thumb/sg01.jpg" draggable="false">
+            </a>
+			<a href="#" class="tl-node" draggable="false"
+               data-thumb="assets/timelines-photo/al/al01.jpg"
+               data-title="1st Album"
+               data-sub="Good-bye My Loneliness"
+               data-released="1991.03.27"
+               data-label="POCH-1082">
+                <img src="assets/timelines-photo/al/al-thumb/al01.jpg" draggable="false">
+            </a>
+            <a href="single.html#sg02" class="tl-node" draggable="false"
+               data-thumb="assets/timelines-photo/sg/sg02.jpg"
+               data-title="2nd Single"
+               data-sub="Fushigi ne..."
+               data-released="1991.06.25"
+               data-label="PODH-1040"
+			   data-tieup='"Magical Zunou Power!!" Theme Song'>
+                <img src="assets/timelines-photo/sg/sg-thumb/sg02.jpg" draggable="false">
+            </a>
+			<a href="single.html#sg03" class="tl-node" draggable="false"
+               data-thumb="assets/timelines-photo/sg/sg03.jpg"
+               data-title="3rd Single"
+               data-sub="Mou Sagasanai"
+               data-released="1991.11.06"
+               data-label="PODH-1060"
+			   data-tieup='"Shichinin no Onna Bengoshi Season 2" Theme Song'>
+                <img src="assets/timelines-photo/sg/sg-thumb/sg03.jpg" draggable="false">
+            </a>
+			<a href="#" class="tl-node" draggable="false"
+               data-thumb="assets/timelines-photo/al/al02.jpg"
+               data-title="2nd Album"
+               data-sub="Mou Sagasanai"
+               data-released="1991.12.25"
+               data-label="POCH-1111">
+                <img src="assets/timelines-photo/al/al-thumb/al02.jpg" draggable="false">
+            </a>
+        </div>
+    </div>
+
+    <div class="tl-col">
+        <div class="tl-year-text">1992</div>
+        <div class="tl-nodes-container">
+            <div class="tl-node empty"></div>
+            <div class="tl-node empty"></div>
+            <a href="#" class="tl-node" draggable="false"
+               data-thumb="assets/timelines-photo/thumb/1992.jpg"
+               data-title="2nd Album"
+               data-sub="Hold Me"
+               data-released="1992.09.02"
+               data-label="POCH-1083">
+                <img src="assets/timelines-photo/icon/1992.jpg" draggable="false">
+            </a>
+            <div class="tl-node empty"></div>
+        </div>
+    </div>
+
+    <div class="tl-col"><div class="tl-year-text">1993</div><div class="tl-nodes-container"><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div></div></div>
+    <div class="tl-col"><div class="tl-year-text">1994</div><div class="tl-nodes-container"><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div></div></div>
+    <div class="tl-col"><div class="tl-year-text">1995</div><div class="tl-nodes-container"><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div></div></div>
+    <div class="tl-col"><div class="tl-year-text">1996</div><div class="tl-nodes-container"><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div></div></div>
+	<div class="tl-col"><div class="tl-year-text">1997</div><div class="tl-nodes-container"><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div></div></div>
+	<div class="tl-col"><div class="tl-year-text">1998</div><div class="tl-nodes-container"><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div></div></div>
+	<div class="tl-col"><div class="tl-year-text">1999</div><div class="tl-nodes-container"><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div><div class="tl-node empty"></div></div></div>
+</div>
+`;
